70cc4580af6db5f65efdb598c72f6b381f0a457a
Allan Lavell <alavell@gmail.com>
2025-08-18 08:58:30 -0300

    Section 2 #10 Layout, Constants, Metadata.

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Nettle Shop";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Conscious clothing shop";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
